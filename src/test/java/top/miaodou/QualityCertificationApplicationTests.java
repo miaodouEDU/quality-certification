a136d738875c95bac261388b382f972c3336d3d5
Simmons <postmaster@miaodou.top>
2019-08-01 23:31:48 +0800

    first commit!

---
+++ src/test/java/top/miaodou/QualityCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package top.miaodou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
